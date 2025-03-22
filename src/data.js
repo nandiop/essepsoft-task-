5892e0791afec3c8b80d7e249fde7a31fd085434
nandiop <su.nandi2002@gmail.com>
2025-03-22 20:18:36 +0530

    Initial commit: Added pagination component with Khand font integration

---
+++ src/data.js
@@ -0,0 +1,53 @@
+
+
+export const data = [
+    {
+        tabIcon: "https://cellesta.in/wp-content/themes/cellesta-theme/img/Untitled-1.png-logo-3-01-1024x373.webp",
+        img: "https://cellesta.in/wp-content/themes/cellesta-theme/img/one-mg1 (1).webp",
+        heading: "Patio (Ground Floor)",
+        subHeading: "Patio is designed for tranquility and leisure with natural landscapes, wellness areas, and various activity zones for all ages.",
+        icons: [
+          { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/mandir.png", text: "mandir" },
+          { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/playtime.png", text: "Kids' Play Area" },
+          { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/jogging.png", text: "Jogging Track" },
+          { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/buddhist-yoga-pose.png", text: "Yoga Deck" },
+          { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/pond.png", text: "Natural Water Body" },
+          { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/walking-man.png", text: "Elevated Walking Area" },
+          { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/target.png", text: "Adda Zone" },
+          { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/fishing.png", text: "Fishing Deck" },
+        ],
+      },
+    {
+      tabIcon: "https://cellesta.in/wp-content/themes/cellesta-theme/img/Untitled-1.png-logo-1-01-1024x451.webp",
+      img: "https://cellesta.in/wp-content/themes/cellesta-theme/img/one-mg2 (1).webp",
+      heading: "Club Azura (3rd Floor)",
+      subHeading: "Club Azura offers a vibrant lifestyle with indoor and outdoor recreational facilities, fitness zones, and community spaces designed for relaxation and socializing.",
+      icons: [
+        { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/swimming-pool1.png", text: "Swimming Pool & Kids Pool" },
+        { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/hall1.png", text: "AC Community Hall with Party Lawn" },
+        { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/console.png", text: "Indoor Games Room" },
+        { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/tennis-racket.png", text: "Badminton Court" },
+        { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/audience.png", text: "Theatre Room" },
+        { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/beach-cabana.png", text: "Landscaped Deck Zone with Cabana" },
+        { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/buddhist-yoga-pose.png", text: "Yoga & Zumba Room" },
+        { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/open-book.png", text: "Library & Cards Room" },
+      ],
+    },
+    {
+      tabIcon: "https://cellesta.in/wp-content/themes/cellesta-theme/img/Untitled-1.png-logo-2-01-1024x456.webp",
+      img: "https://cellesta.in/wp-content/themes/cellesta-theme/img/one-mg (1).webp",
+      heading: "Panorama (Roof Level)",
+      subHeading: "Panorama provides breathtaking views and a unique experience with elevated fitness areas, outdoor entertainment spaces, and serene relaxation zones.",
+      icons: [
+        { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/twin-towers.png", text: "Skydeck - 320ft above the ground level" },
+        { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/barbeque.png.crdownload", text: "Barbeque Zone" },
+        { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/dumbbell.png", text: "Outdoor Gym" },
+        { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/telescope.png", text: "Telescope Deck" },
+        { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/couple.png", text: "Elders' Adda Zone" },
+        { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/moon-gazing.png", text: "Star Gazing Area" },
+        { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/lounge.png", text: "Open Lounge" },
+        { src: "https://cellesta.in/wp-content/themes/cellesta-theme/img/reflexology.png", text: "Foot Reflexology Walk" },
+      ],
+    },
+  ];
+  
